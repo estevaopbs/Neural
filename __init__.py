9607d9f232ca53704989e3bca3adc3577a36f18e
sbb <estevaopbsantos@gmail.com>
2023-11-17 19:27:42 -0300

    Complete refactoring

--- __init__.py
+++ __init__.py
@@ -1,3 +1,16 @@
-from .source import *
+from .source import (
+    Crossover,
+    Layer,
+    Network,
+    Neuron,
+    NeuronDerivative,
+    NeuronFunction,
+    NeuronRandomGenerator,
+)
 
 # from .tests import *
+
+__title__ = "Neural"
+__version__ = "2.0"
+__author__ = "sbb"
+__doc__ = "https://github.com/estevaopbs/Neural"
--- source/__init__.py
+++ source/__init__.py
@@ -1,10 +1,6 @@
-import source.enums.NeuronFunction as NeuronFunction
-import source.enums.NeuronRandomGenerator as NeuronRandomGenerator
+from source.enums.NeuronDerivative import NeuronDerivative
+from source.enums.NeuronFunction import NeuronFunction
+from source.enums.NeuronRandomGenerator import NeuronRandomGenerator
 
 from .cases import Crossover
 from .entities import Layer, Network, Neuron
-
-__title__ = "Neural"
-__version__ = "2.0"
-__author__ = "sbb"
-__doc__ = "https://github.com/estevaopbs/Neural"
